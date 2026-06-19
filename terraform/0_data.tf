6ea22cb6779ac41440c15b4e4973ddb4bc58bd34
Carlos Solis Salazar <carlos@soysoliscarlos.com>
2026-06-19 13:29:34 -0500

    add data

--- terraform/0_data.tf
+++ terraform/0_data.tf
@@ -8,6 +8,8 @@
 # - AI Foundry Hub: ai_foundry_hub.tf
 # - AI Foundry Project: ai_foundry_project.tf
 
+data "azurerm_client_config" "current" {}
+
 data "azurerm_resource_group" "this" {
   name = var.resource_group_name
 }
